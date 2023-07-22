acc9d4ea2eccb7c7f432dbbbcae19bc9b0abda9c
Bruno Nascimento <bruno.hn@icloud.com>
2023-07-22 20:53:03 -0300

    refactor: add html_to_string macro

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,40 @@
 use axum::{response::Html, routing::get, Router};
+use html_to_string_macro::html;
 
 enum Children {
     String(String),
-    Html(Html<String>),
-    Function(fn(children: Children) -> Html<String>),
+    Function(fn(children: Children) -> String),
 }
 
 fn resolve_children(children: Children) -> String {
     match children {
         Children::String(s) => s,
-        Children::Html(h) => h.0,
-        Children::Function(f) => f(Children::String("Hello, world!".to_string())).0,
+        Children::Function(f) => f(Children::String("".to_string())),
     }
 }
 
-fn layout(children: Children) -> Html<String> {
-    Html(format!(
-        r#"
+fn layout(children: Children) -> String {
+    html!(
         <!DOCTYPE html>
         <html>
             <head>
-                <title>rust components</title>
+                <title>"rust components"</title>
             </head>
             <body>
                 <div style="display: flex; flex-direction: column; align-items: center">
-                    {}
+                    {resolve_children(children)}
                 </div>
             </body>
         </html>
-    "#,
-        resolve_children(children)
-    ))
+    )
 }
 
-fn heading(children: Children) -> Html<String> {
-    Html(format!("<h1>{}</h1>", resolve_children(children)))
+fn heading(children: Children) -> String {
+    html!(
+      <h1>
+          {resolve_children(children)}
+      </h1>
+    )
 }
 
 #[tokio::main]
@@ -43,11 +43,11 @@ async fn main() {
 
     let page = layout(Children::Function(|_children| {
         heading(Children::Function(|_| {
-            Html(format!("Hello world from a component!"))
+            "Hello world from a component!".to_string()
         }))
     }));
 
-    let app = Router::new().route("/", get(|| async { page }));
+    let app = Router::new().route("/", get(|| async { Html(page) }));
 
     // run it with hyper on localhost:3100
     axum::Server::bind(&"0.0.0.0:3100".parse().unwrap())
